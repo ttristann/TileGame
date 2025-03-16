6fb806012c8450b88b42c66e02878044ffe451f8
kyla-127 <107767149+kyla-127@users.noreply.github.com>
2025-03-15 19:58:25 -0700

    updated timer

--- TMGE/test_game_ui.py
+++ TMGE/test_game_ui.py
@@ -5,7 +5,6 @@ def main():
     root = tk.Tk()
     root.title("Bejeweled Test")
     ui = GameUI(root, on_exit_game=root.quit)
-    ui.start_bejeweled()  # Automatically start Bejeweled
     root.mainloop()
 
 if __name__ == "__main__":
